141c442e03926ac932861eab61ec8649c3314366
mayankbisht01 <mayankrajput2480@gmail.com>
2026-06-30 18:40:18 +0530

    all types in one file

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Posts {
+    _id: string;
+    content: string;
+    image?: string;
+    author: { _id: string; name: string; avatar?: string };
+    likes: string[];
+    comments: { _id: string, text: string, user: string }[];
+    createdAt: string;
+}
+
+export type Props =  {
+    children: React.ReactNode;
+};
