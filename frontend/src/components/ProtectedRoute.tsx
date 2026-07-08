e8d99fadcaf2723215114535c13a3f3f45176a43
mayankbisht01 <mayankrajput2480@gmail.com>
2026-07-08 18:22:07 +0530

    fix: content only creatPost bug

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,9 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 import type { Props } from "../types";
+import Loading from "./Loading";
 
 export default function ProtectedRoute({ children }: Props) {
-    const { accessToken } = useAuth();
+    const { accessToken, authLoading } = useAuth();
+
+    if (authLoading) return <Loading/>
     
     if (!accessToken) {
         return <Navigate to="/login" replace />
